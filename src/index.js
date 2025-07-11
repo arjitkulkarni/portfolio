cbe17518af0a1c0db273633b1861d09dbc516a1e
Patel-Muhumad <arjitkulkarni81018@gmail.com>
2025-07-11 20:04:49 +0530

    Improve mobile navbar and interactivity

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { motion } from 'framer-motion';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
